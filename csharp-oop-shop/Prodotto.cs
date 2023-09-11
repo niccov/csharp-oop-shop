279e5ea0edf2812ff1b59346c3d1eea7512669f8
niccov <niccoventuri27@gmail.com>
2023-09-12 00:37:19 +0200

    Aggiungere i file di progetto.

---
+++ csharp-oop-shop/Prodotto.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop
+{
+    internal class Prodotto
+    {
+        //PROPRIETA' e STATI
+        public int Codice { get; private set; }
+        public string Nome { get; set; }
+        public string Descrizione { get; set; }
+        public double Prezzo { get; set; }
+        public int Iva { get; set; }
+
+
+        //COSTRUTTORI​
+        public Prodotto(string nome, string descrizione, double prezzo, int iva)
+        {
+            this.Codice = new Random().Next(0, 99999999);
+            this.Nome = nome;
+            this.Descrizione = descrizione;
+            this.Prezzo = prezzo;
+            this.Iva = iva;
+        }
+
+
+        //GETTERS​
+       public double GetPrezzoBase()
+        {
+            return this.Prezzo;
+        }
+
+        public double GetPrezzoIva()
+        {
+            return Prezzo + (Prezzo / 100 * Iva);
+
+        }
+
+        public string GetNomeCodice()
+        {
+            return this.Codice + " " + this.Nome;
+        }
+    }
+}
---
+++ csharp-oop-shop/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using csharp_oop_shop;
+
+Prodotto brunello = new Prodotto("Brunello di Montalcino", "vino con uvaggio San Giovese", 20, 22);
+
+Prodotto tuaRita = new Prodotto("Rosso dei Notri", "vino con uvaggi San Giovese, Cabernet e Merlot", 25, 22);
+
+Prodotto renis = new Prodotto("Renis", "vino di Syrah in purezza", 22, 22);
+
+Prodotto bulichella = new Prodotto("Tuscanio", "vino con uvaggio Vermentino", 15, 22);
+
+Prodotto terradonna = new Prodotto("Bixbi", "vino con uvaggi Syrah e San Giovese", 16, 22);
+
+
+//array prodotti
+
+Prodotto[] arrayProdotti = new Prodotto[] { brunello, tuaRita, renis, bulichella, terradonna };
+
+//chiamo la funzione per la stampa dell'array
+
+StampaArrayProdotti(arrayProdotti);
+
+//Funzioni
+
+static void StampaArrayProdotti(Prodotto[] arrayProdotti)
+{
+    foreach (Prodotto prodotto in arrayProdotti)
+    {
+        Console.WriteLine($"Codice prodotto: {prodotto.Codice}");
+        Console.WriteLine($"Nome esteso: {prodotto.GetNomeCodice()}");
+        Console.WriteLine($"Nome prodotto: {prodotto.Nome}");
+        Console.WriteLine($"Descrizione del prodotto: {prodotto.Descrizione}");
+        Console.WriteLine($"Prezzo: {prodotto.GetPrezzoBase()} euro");
+        Console.WriteLine($"Prezzo con iva: {prodotto.GetPrezzoIva()} euro");
+        Console.WriteLine();
+    }
+}
